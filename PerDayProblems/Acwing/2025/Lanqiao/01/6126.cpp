fa4f4310a8edd619c54a28b82fed7d488d259d49
hbq2004 <1032816215@qq.com>
2025-02-17 21:36:00 +0800

    6126.cpp

---
+++ PerDayProblems/Acwing/2025/Lanqiao/01/6126.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, f;
+    string s;
+    cin >> n >> f >> s;
+
+    
+
+    return 0;
+}
