5bac6ff3c69044d1e652079fab44511e7460ed8b
hbq2004 <1032816215@qq.com>
2025-02-17 13:59:24 +0800

    Acwing 6122 Lanqiao Tranning Week 1

--- PerDayProblems/Acwing/2025/Lanqiao/01/6122.cpp
+++ PerDayProblems/Acwing/2025/Lanqiao/01/6122.cpp
@@ -5,7 +5,26 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int n, q;
+    cin >> n >> q;
+
+    vector<vector<vector<int>>> cnt(3, vector<vector<int>>(n, vector<int>(n)));
+
+    int ans = 0;
+    while (q--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (++cnt[0][x][y] == n) {
+            ans++;
+        }
+        if (++cnt[1][y][z] == n) {
+            ans++;
+        }
+        if (++cnt[2][x][z] == n) {
+            ans++;
+        }
+        cout << ans << '\n';
+    }
 
     return 0;
 }
